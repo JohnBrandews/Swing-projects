43df235ab5bd64acac3deb4e50566eb9483dc548
john Brandews <johnbrandews@gmail.com>
2024-09-16 22:29:10 +0300

    added a few files

--- Mbutton.java
+++ Mbutton.java
@@ -6,12 +6,21 @@ import java.awt.event.ActionEvent;
 import javax .swing.*;
 
 public class Mbutton extends JFrame  implements ActionListener{
-    //we declared the button outside the constructor to give it a global reference
+    //we declared the button and label outside the constructor to give it a global reference
     JButton button;
+    JLabel label;
     Mbutton(){
+        ImageIcon icon = new ImageIcon("download.jpeg");
+        label = new JLabel();
+        label.setIcon(icon);
+        label.setBounds(150,250,250,250);
+        label.setVisible(false);//prevents the label from appearing befor it's clicked
          button = new JButton();
         button.setBounds(200,100,100,50);
         button.addActionListener(this);
+        button.setText("click me");
+        button.setFocusable(false);//removes the box that sorrounds the text inside the button
+        // button.setEnabled(false);//disables the button when  set to false
         //uses a lambda expression but is a short form no need of long syntax action listeners
         // button.addActionListener(e -> System.out.println("welcome"));
         this.setTitle("my window");
@@ -20,11 +29,13 @@ public class Mbutton extends JFrame  implements ActionListener{
         this.setSize(500,500);
         this.setVisible(true);
         this.add(button);
+        this.add(label);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==button){
-            System.out.println("welcome");
+            // System.out.println("welcome");
+            label.setVisible(true);
         }
     }
 }
