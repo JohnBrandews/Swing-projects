0dc2b0ab28e1b6951a7c3046f8eeb7e16343278c
john Brandews <johnbrandews@gmail.com>
2024-09-26 01:19:09 +0300

    added a few modifications to enable drag and drop in swing

--- Mdragpanel.java
+++ Mdragpanel.java
@@ -1,9 +1,10 @@
 import java.awt.*;
 import javax .swing.*;
 import java.awt.Graphics;
-import java.awt.MouseAdapter;
-import java.awt.MouseMotionAdapter;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
 public class Mdragpanel extends JPanel{
     ImageIcon image = new ImageIcon("download.jpeg");
     final int WIDTH = image.getIconWidth();
@@ -11,15 +12,30 @@ public class Mdragpanel extends JPanel{
     Point imageCorner;
     Point prevPt;
     Mdragpanel(){
-
+        imageCorner = new Point(0,0);
+        ClickListener clicklistener = new ClickListener();
+        DragListener draglistener = new DragListener();
+        this.addMouseListener(clicklistener);
+        this.addMouseMotionListener(draglistener);
     }
     public void paintComponent (Graphics g) {
-
+        super.paintComponent(g);
+        image.paintIcon(this,g,(int)imageCorner.getX(),(int)imageCorner.getY());
     }
     private class ClickListener extends MouseAdapter{
-
+        public void mousePressed(MouseEvent e){
+            prevPt = e.getPoint();
+        }
     }
     private class DragListener extends MouseMotionAdapter{
-
+        public void mouseDragged(MouseEvent e) {
+            Point currentPt = e.getPoint();
+            imageCorner.translate(
+                (int)(currentPt.getX() - prevPt.getX()),
+                (int)(currentPt.getY() - prevPt.getY())
+            );
+            prevPt = currentPt;
+            repaint();
+        }
     }
 }
